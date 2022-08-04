cf76668dcbd19caf0ada73f0dcc857e3d785fef3
unknown <abdulkarimansari2000@gmail.com>
2022-08-04 18:53:26 +0530

    port fixed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, "./public");
 const connectDB = require("./mongoconnection");
 const router = require("./routes/route");
